35aedc1948d8ef8ab0abfcb7e882a0c55a067bcf
Bardia <iconiccerberus@gmail.com>
2026-06-09 17:01:44 +0330

    Fix GitHub Pages setup for mirrored repositories

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite"; 
+import process from "node:process";
 
 export default defineConfig(({command}) => ({
-  base: command === "build" ? "/innotech-website/" : "/",
+  base: command === "build"
+    ? `/${process.env.GITHUB_REPOSITORY?.split("/").pop() ?? "innotech-website"}/`
+    : "/",
   plugins: [
     react(),
     tailwindcss(), 
