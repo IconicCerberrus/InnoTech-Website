41f8880ee1526d69cb9b8a170182a2ec597dee86
Bardia <iconiccerberus@gmail.com>
2026-06-10 11:53:22 +0330

    fix pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: "/InnoTech-Website/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "VITE_");
+
+  return {
+    base: env.VITE_BASE_PATH || "/",
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+  };
 });
